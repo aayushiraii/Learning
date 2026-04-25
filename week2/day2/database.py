427f1b388ce20298b90e8bc6e59ebb8182664c27
Aayushi <aayushirai@bitcot.com>
2026-04-25 11:39:39 +0530

    Updated with docstr and .evn

--- week2/day2/database.py
+++ week2/day2/database.py
@@ -1,10 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/testdb"
+load_dotenv()  # load .env file
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- week2/day2/main.py
+++ week2/day2/main.py
@@ -21,24 +21,45 @@ def get_db():
 
 @app.get("/")
 def home():
+    """
+    Root endpoint to verify API is running.
+
+    Returns:
+        dict: Simple status message.
+    """
     return {"message": "API is running"}
 
 
 # CREATE
 @app.post("/users/")
 def create_user(name: str, email: str, db: Session = Depends(get_db)):
+    """Creates a new User
+    Returns :
+    The new user that has been added
+    """
     return crud.create_user(db, name, email)
+
 
 
 # READ ALL
 @app.get("/users/")
 def get_users(db: Session = Depends(get_db)):
+    """Reads all the users in the Database
+    
+    Returns:
+    Gives all the users
+    """
     return crud.get_users(db)
 
 
 # READ ONE
 @app.get("/users/{user_id}")
 def get_user(user_id: int, db: Session = Depends(get_db)):
+    """Reads only specific Users with the requested Users Id
+    
+    Returns :
+    The requested user Id
+    """
     user = crud.get_user(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -48,6 +69,11 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
 # UPDATE
 @app.put("/users/{user_id}")
 def update_user(user_id: int, name: str = None, email: str = None, db: Session = Depends(get_db)):
+    """Updates the existing user
+    
+    returns:
+    The updated user in place of the previous user
+      """
     user = crud.update_user(db, user_id, name, email)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -57,7 +83,14 @@ def update_user(user_id: int, name: str = None, email: str = None, db: Session =
 # DELETE
 @app.delete("/users/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
+    """Deletes the requested user
+    
+    returns:
+    User deleted message
+    
+    """
     user = crud.delete_user(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return {"message": "User deleted"}
+
