2fcd66a61e6c2a1f1662326b3c6d0f03b53bb29f
Aayushi <aayushirai@bitcot.com>
2026-05-05 12:09:58 +0530

    week3 day1

---
+++ week3/day1/main.py
@@ -0,0 +1,193 @@
+from fastapi import FastAPI, Depends, HTTPException, Header
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+import models, schemas, crud
+from database import engine, get_db   
+
+# create tables
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="FastAPI Full App")
+
+
+#check
+def get_current_user(token: str = Header(None)):
+    if token != "secret123":
+        raise HTTPException(status_code=401, detail="Not logged in")
+
+
+#user
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    try:
+        new_user = crud.create_user(db, user)
+
+        if not new_user:
+            raise HTTPException(status_code=400, detail="Email already exists")
+
+        return new_user
+
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+
+@app.post("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = crud.authenticate_user(db, user.email, user.password)
+
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    return {"token": "secret123"}
+
+
+@app.get("/users", response_model=list[schemas.UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user(db, user_id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
+
+
+@app.put("/users/{user_id}", response_model=schemas.UserResponse)
+def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
+    updated_user = crud.update_user(db, user_id, user)
+
+    if not updated_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return updated_user
+
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+
+    return {"message": "User deleted"}
+
+
+#category
+@app.post("/categories")
+def create_category(
+    data: schemas.CategoryCreate,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)
+):
+    category = crud.create_category(db, data.name)
+
+    if not category:
+        raise HTTPException(status_code=400, detail="Category exists")
+
+    return category
+
+
+@app.get("/categories")
+def get_categories(db: Session = Depends(get_db)):
+    return db.query(models.Category).all()
+
+
+@app.put("/categories/{category_id}")
+def update_category(
+    category_id: int,
+    data: schemas.CategoryCreate,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)   # protect update
+):
+    updated = crud.update_category(db, category_id, data.name)
+
+    if not updated:
+        raise HTTPException(status_code=404, detail="Category not found")
+
+    return updated
+
+
+@app.delete("/categories/{category_id}")
+def delete_category(
+    category_id: int,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)   # protect delete
+):
+    category = db.query(models.Category).filter(models.Category.id == category_id).first()
+
+    if not category:
+        raise HTTPException(status_code=404, detail="Category not found")
+
+    db.delete(category)
+    db.commit()
+
+    return {"message": "Category deleted"}
+
+
+#items
+@app.post("/categories/{category_id}/items")
+def create_item(
+    category_id: int,
+    item: schemas.ItemCreate,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)
+):
+    new_item = crud.create_item(db, item, category_id)
+
+    if not new_item:
+        raise HTTPException(status_code=400, detail="Item exists")
+
+    return new_item
+
+
+@app.get("/items")
+def get_items(db: Session = Depends(get_db)):
+    return db.query(models.Item).all()
+
+
+@app.get("/categories/{category_id}/items")
+def get_category_items(category_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Item).filter(models.Item.category_id == category_id).all()
+
+
+@app.put("/items/{item_id}")
+def update_item(
+    item_id: int,
+    item: schemas.ItemCreate,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)   # protect update
+):
+    updated = crud.update_item(db, item_id, item)
+
+    if not updated:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return updated
+
+
+@app.delete("/items/{item_id}")
+def delete_item(
+    item_id: int,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)   # protect delete
+):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    db.delete(item)
+    db.commit()
+
+    return {"message": "Item deleted"}
+
+
